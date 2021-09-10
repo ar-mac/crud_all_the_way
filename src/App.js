df1d9f74f1a64aabf7861095b50e283274337be3
Arkadiusz Machalica <a.machalica@selleo.com>
2021-09-10 17:29:21 +0200

    Setup routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
+import './App.css'
+import { Router } from '@reach/router'
+
+const Comp = () => {
+  return <div>Comp</div>
+}
 
 function App() {
   return (
-    <div>
-    </div>
-  );
+    <Router>
+      <Comp path='/users' />
+      <Comp path='/users/featured' />
+      <Comp path='/users/:userId' />
+      <Comp path='/users/:userId/posts' />
+      <Comp path='/posts' />
+      <Comp path='/posts/featured' />
+      <Comp path='/posts/:postId' />
+      <Comp path='/' />
+    </Router>
+  )
 }
 
-export default App;
+export default App
