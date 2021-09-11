7b9a632782e24bc564ce2903dd035cf7e828831e
Arkadiusz Machalica <a.machalica@selleo.com>
2021-09-11 18:16:08 +0200

    Setup routes and some UI

--- src/App.js
+++ src/App.js
@@ -1,22 +1,59 @@
 import './App.css'
-import { Router } from '@reach/router'
+import 'antd/dist/antd.css'
+import { Link, Router, useLocation } from '@reach/router'
+import { Card, Layout, Menu } from 'antd'
+
+const { Content, Header } = Layout
 
 const Comp = () => {
-  return <div>Comp</div>
+  const { pathname } = useLocation()
+
+  return (
+    <div>
+      <Card title={pathname} style={{ width: 300 }} />
+    </div>
+  )
 }
 
 function App() {
   return (
-    <Router>
-      <Comp path='/users' />
-      <Comp path='/users/featured' />
-      <Comp path='/users/:userId' />
-      <Comp path='/users/:userId/posts' />
-      <Comp path='/posts' />
-      <Comp path='/posts/featured' />
-      <Comp path='/posts/:postId' />
-      <Comp path='/' />
-    </Router>
+    <Layout>
+      <Header>
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+          <Menu.Item>
+            <Link to="/">Dashboard</Link>
+          </Menu.Item>
+          <Menu.Item>
+            <Link to="/users">Users</Link>
+          </Menu.Item>
+          <Menu.Item>
+            <Link to="/users/featured">Featured users</Link>
+          </Menu.Item>
+          <Menu.Item>
+            <Link to="/posts">Posts</Link>
+          </Menu.Item>
+          <Menu.Item>
+            <Link to="/posts/featured">Featured Posts</Link>
+          </Menu.Item>
+        </Menu>
+      </Header>
+      <Content style={{ padding: '0 50px' }}>
+        <Layout style={{ padding: '24px 0', backgroundColor: '#fff' }}>
+          <Content style={{ padding: '0 24px', minHeight: 280 }}>
+            <Router>
+              <Comp path="/users" />
+              <Comp path="/users/featured" />
+              <Comp path="/users/:userId" />
+              <Comp path="/users/:userId/posts" />
+              <Comp path="/posts" />
+              <Comp path="/posts/featured" />
+              <Comp path="/posts/:postId" />
+              <Comp path="/" />
+            </Router>
+          </Content>
+        </Layout>
+      </Content>
+    </Layout>
   )
 }
 
