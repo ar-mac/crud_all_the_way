04624066930544ebb71112b9e2979af6b9ac5967
Arkadiusz Machalica <a.machalica@selleo.com>
2021-10-09 13:30:56 +0200

    Add adjustable limit to featured users

--- src/routes/FeaturedUsers/FeaturedUsers.js
+++ src/routes/FeaturedUsers/FeaturedUsers.js
@@ -1,20 +1,24 @@
-import { List, Typography } from 'antd'
+import { InputNumber, List, Space, Spin, Typography } from 'antd'
 import { Link } from '@reach/router'
 import { useGetFilteredUsers } from '../../api/users'
+import { useState } from 'react'
 
 const isFeatured = true
-const limit = 3
 
 export const FeaturedUsers = () => {
+  const [_limit, _setLimit] = useState(3)
   // fetch users filtered by isFeatured = true and limit to 3
-
-  const { data, isLoading } = useGetFilteredUsers({
-    params: { isFeatured, limit },
+  const { data, isLoading, isFetching } = useGetFilteredUsers({
+    params: { isFeatured, _limit },
   })
 
   return (
     <>
       <Typography.Title level={2}>Featured Users</Typography.Title>
+      <Space>
+        <InputNumber min={1} max={50} value={_limit} onChange={_setLimit} />
+        {isFetching && <Spin size="small" />}
+      </Space>
       <List
         loading={isLoading}
         itemLayout="horizontal"
